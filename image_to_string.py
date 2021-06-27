c4f8ba56ed516004f5748a22176a75b7bef7ac89
Diwakar Ghosh <68577575+Diwakar-debug@users.noreply.github.com>
2021-06-27 15:43:02 +0530

    Update image_to_string.py

--- image_to_string.py
+++ image_to_string.py
@@ -5,4 +5,4 @@ from PIL import Image
 #Importing the image using Pillow
 image = Image.open(r'filename') #It is always good to provide the full path of the image as a raw string, afterall you might never know why it dosen't compile :)
 text = pt.image_to_string(image)
-print(text
+print(text)
