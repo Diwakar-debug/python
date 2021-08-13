7ead82bad1a74a050d02ab06ba7e27db18c4205e
Diwakar Ghosh <68577575+Diwakar-debug@users.noreply.github.com>
2021-08-13 07:51:17 +0530

    Delete image_to_string.py

--- image_to_string.py
+++
@@ -1,8 +0,0 @@
-#Libraries required for the given operation
-import pytesseract as pt
-from PIL import Image
-
-#Importing the image using Pillow
-image = Image.open(r'filename') #It is always good to provide the full path of the image as a raw string, afterall you might never know why it dosen't compile :)
-text = pt.image_to_string(image)
-print(text)
